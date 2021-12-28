3031ffe1fd6a8d8dd808306fe327768af8079e22
Adam Poustka <poustka.a@gmail.com>
2021-12-28 13:28:16 +0100

    Add basic implementaion of HomeVC (WorkoutVC)

--- WorkoutTracker/Controller/HomeViewController.swift
+++ WorkoutTracker/Controller/HomeViewController.swift
@@ -8,12 +8,42 @@
 import UIKit
 
 class HomeViewController: UIViewController {
-
+    
+    // Outlets
+    @IBOutlet weak var tableView: UITableView!
+    
+    // Model variables
+    var workouts: [WorkoutModel] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupTableView()
     }
 
+    func setupTableView() {
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.tableFooterView = UIView()
+    }
 
+    @IBAction func addButtonPressed(_ sender: Any) {
+    }
 }
-
+//MARK: - TableViewDelegate
+extension HomeViewController: UITableViewDelegate {
+}
+//MARK: - TableViewDataSource
+extension HomeViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        workouts.count != 0 ? workouts.count : 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        if workouts.count == 0 {
+            cell.textLabel?.text = "Please add workouts"
+        }
+        return cell
+    }
+    
+}
